1c7313c2e9383350eb2187127a3ef36b1ce87414
choiyunsung_100 <12345ggh81@gmail.com>
2021-04-01 21:13:42 +0900

    Context_

--- app/src/main/java/com/example/myapplication/Context.kt
+++ app/src/main/java/com/example/myapplication/Context.kt
@@ -1,5 +1,6 @@
 package com.example.myapplication
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -9,5 +10,6 @@ class Context : AppCompatActivity() {
         setContentView(R.layout.activity_context)
 
         val context : Context = this // 모든 클레스는 결국 Context의 자식클레스이기때문에 this를 할당 시켜줘도 무관하다.
+        val applicationContext: Context = getApplicationContext() as Context
     }
 }
