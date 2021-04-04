17bd5f6cca8c85375ef11afa9a1c2cab3e0b1281
choiyunsung_100 <12345ggh81@gmail.com>
2021-04-04 18:15:17 +0900

    contacts_

--- app/src/main/java/com/example/myapplication/Contacts.kt
+++ app/src/main/java/com/example/myapplication/Contacts.kt
@@ -3,7 +3,10 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.view.LayoutInflater
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.viewpager.widget.ViewPager
 import com.example.myapplication.R
@@ -18,5 +21,29 @@ class Contacts : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contacts)
         binding1 = DataBindingUtil.setContentView(this, R.layout.activity_contacts)
+
+        val Phonebook = ArrayList<PhonebookTyipe>()
+        for(i in 0 until 10)
+        {
+            Phonebook.add(PhonebookTyipe(""+i + "님","010-6987-156"+i))
+        }
+
+        val container = findViewById<LinearLayout>(R.id.container)
+        val infilter = LayoutInflater.from(this@Contacts)
+
+        for(i in 0 until Phonebook.size){
+            val itemView = infilter.inflate(R.layout.contacts_item, null)
+            val NameView = itemView.findViewById<TextView>(R.id.name)
+            val numberView = itemView.findViewById<TextView>(R.id.number)
+
+            NameView.setText(Phonebook.get(i).name)
+            numberView.setText(Phonebook.get(i).callnumber)
+            //addView로 보여지게 설정
+            container.addView(itemView)
+        }
     }
+}
+
+class PhonebookTyipe(val name : String, val callnumber : String){
+
 }
